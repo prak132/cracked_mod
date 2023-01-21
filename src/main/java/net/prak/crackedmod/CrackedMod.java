2074b52f471537efbb8321543fa19f9ecbb34d0d
Prak <100105525+prak132@users.noreply.github.com>
2023-01-20 18:10:15 -0800

    Initial commit

---
+++ src/main/java/net/prak/crackedmod/CrackedMod.java
@@ -0,0 +1,15 @@
+package net.prak.crackedmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrackedMod implements ModInitializer {
+	public static final String MOD_ID = "crackedmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/prak/crackedmod/CrackedModClient.java
@@ -0,0 +1,11 @@
+package net.prak.crackedmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrackedModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
